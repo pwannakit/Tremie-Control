ab387df25dde35f553f50a9721bc282c98397ee4
pwannakit <pwannakit@gmail.com>
2026-05-08 10:47:07 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,23 @@
-const CACHE_NAME = 'Tremie-v40';
+const CACHE_NAME = 'tremie-control-v40-cache';
 const ASSETS = [
-  './',
-  './index.html',
+  'index.html',
+  'manifest.json',
   'https://cdn.tailwindcss.com',
+  'https://cdn.jsdelivr.net/npm/chart.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css'
 ];
 
-// ติดตั้ง Service Worker และเก็บไฟล์ลง Cache
+// ติดตั้ง Service Worker และเก็บ Cache
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
   );
 });
 
-// เรียกใช้งานไฟล์จาก Cache เมื่อ Offline
+// ดึงข้อมูลจาก Cache เมื่อ Offline
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
